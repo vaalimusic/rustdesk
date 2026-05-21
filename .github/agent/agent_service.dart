1ed1d715050fa28fb1ad863f8bb77c3bc6814358
Arthur Valiev <63344403+vaalimusic@users.noreply.github.com>
2026-05-21 18:14:36 +0300

    fix(agent): use relative import for generated_bridge.dart

--- .github/agent/agent_service.dart
+++ .github/agent/agent_service.dart
@@ -8,7 +8,7 @@ import 'dart:io';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_hbb/generated_bridge.dart';
+import 'generated_bridge.dart';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
 
